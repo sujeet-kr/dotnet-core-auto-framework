886cde4e13913120ff5bddb71eb5ccbf47478039
Sujeet Kumar <sujeet.kumar@joes-MacBook-Pro-2.local>
2018-05-17 16:21:44 -0400

    first commit

---
+++ tests/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace fc.test.project
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1(){
+            IWebDriver _driver = new ChromeDriver(@"/Users/sujeet.kumar/WebDriver-Drivers/");
+            _driver.Navigate().GoToUrl("http://www.google.com");
+            IWebElement element = _driver.FindElement(By.Name("q"));
+            element.SendKeys("Sujeet");
+            //Thread.Sleep(10000);                      
+        }
+        
+    }
+}
