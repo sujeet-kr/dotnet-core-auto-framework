1d5818bf4ff723b8400ebb73c462b84c890656e5
Sujeet Kumar <sujeet.kumar@joes-MacBook-Pro-2.local>
2018-05-20 23:00:46 -0400

    added Remote webdriver to test

--- tests/Class1.cs
+++ tests/Class1.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 
 namespace fc.test.project
 {
@@ -11,7 +12,12 @@ namespace fc.test.project
     {
         [Test]
         public void TestMethod1(){
-            IWebDriver _driver = new ChromeDriver(@"/Users/sujeet.kumar/WebDriver-Drivers/");
+            DesiredCapabilities capability = new DesiredCapabilities();
+            capability.SetCapability("browserName", "chrome");
+            //IWebDriver _driver = new ChromeDriver(@"/Users/sujeet.kumar/WebDriver-Drivers/");
+            IWebDriver _driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), capability);
+
+
             _driver.Navigate().GoToUrl("http://www.google.com");
             IWebElement element = _driver.FindElement(By.Name("q"));
             element.SendKeys("Sujeet");
