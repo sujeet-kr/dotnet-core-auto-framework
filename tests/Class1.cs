1744bafc16d1d901c351c434baec221e2a205fcc
Sujeet Kumar <sujeet.kumar@joes-MacBook-Pro-2.local>
2018-05-28 22:03:02 -0400

    reduced the test

--- tests/Class1.cs
+++ tests/Class1.cs
@@ -11,16 +11,6 @@ namespace fc.test.project
     {
         [Fact]
         public void TestMethod1(){
-            DesiredCapabilities capability = new DesiredCapabilities();
-            capability.SetCapability("browserName", "chrome");
-            //IWebDriver _driver = new ChromeDriver(@"/Users/sujeet.kumar/WebDriver-Drivers/");
-            IWebDriver _driver = new RemoteWebDriver(new Uri("http://172.20.0.2:4444/wd/hub"), capability);
-//             IWebDriver _driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), capability);
-            
-            _driver.Navigate().GoToUrl("http://www.google.com");
-            IWebElement element = _driver.FindElement(By.Name("q"));
-            element.SendKeys("Sujeet");
-            //Thread.Sleep(10000);    
             Assert.True(1 < 2);
         }
 
