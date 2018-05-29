082083dca125ef9733de1f4d405655f374ce1e75
Sujeet Kumar <sujeet.kr@hotmail.com>
2018-05-28 21:16:08 -0400

    Update Class1.cs

--- tests/Class1.cs
+++ tests/Class1.cs
@@ -10,18 +10,8 @@ namespace fc.test.project
     public class UnitTest1
     {
         [Fact]
-        public void TestMethod1(){
-            DesiredCapabilities capability = new DesiredCapabilities();
-            capability.SetCapability("browserName", "chrome");
-            //IWebDriver _driver = new ChromeDriver(@"/Users/sujeet.kumar/WebDriver-Drivers/");
-            IWebDriver _driver = new RemoteWebDriver(new Uri("http://172.20.0.2:4444/wd/hub"), capability);
-//             IWebDriver _driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), capability);
-            
-            _driver.Navigate().GoToUrl("http://www.google.com");
-            IWebElement element = _driver.FindElement(By.Name("q"));
-            element.SendKeys("Sujeet");
-            //Thread.Sleep(10000);    
-            Assert.True(1 < 2);
+        public void TestMethod2(){
+            Assert.True(1<2);
         }
 
 
